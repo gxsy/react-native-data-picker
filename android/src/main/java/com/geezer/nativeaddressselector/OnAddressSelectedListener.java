8169ac0a19d6804ae6bc95e2a36f657e9b226a21
travelgeezer <travelgeezer@icloud.com>
2017-08-09 17:55:28 +0800

    [T] android: update android module

--- android/src/main/java/com/geezer/nativeaddressselector/OnAddressSelectedListener.java
+++
@@ -1,8 +0,0 @@
-package com.geezer.nativeaddressselector;
-
-
-import com.geezer.nativeaddressselector.model.GeneralData;
-
-public interface OnAddressSelectedListener {
-    void onAddressSelected(GeneralData province, GeneralData city, GeneralData county, GeneralData street);
-}
